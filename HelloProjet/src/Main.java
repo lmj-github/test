fad03566199bfc88952c23946efff46d58e1a5c0
lmj-github <mjsource@outlook.com>
2019-09-09 11:53:06 +0800

    idea提交的

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -1,20 +1,21 @@
-﻿import org.junit.Test;
+import org.junit.Test;
 
 public class Main {
 
     @Test
     public void testHelloWrold() {
 
-        System.out.println("HelloWorld!");
-        System.out.println("新的代码被提交2222123");
-   System.out.println("lisi新的代码被提交2222123");
-<<<<<<< HEAD
-        System.out.println("我修改");
-=======
-  System.out.println("lisi修改");
->>>>>>> 6724dbb93355747f83dd24316befcd54ffa540a9
- System.out.println("lisi合并");
- System.out.println("lisi发23");
+            System.out.println("HelloWorld!");
+
+       System.out.println("lisi新的代码被提交2222123");
+
+            System.out.println("我修改");
+
+      System.out.println("lisi修改");
+
+     System.out.println("lisi合并");
+     System.out.println("lisi发23");
+            System.out.println("idea提交的");
 
 
 
